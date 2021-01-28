54ddd53aab8a34a03e9d111bf8eb3f1a5a4a8433
James Schofield <jschofield1@eagles.ewu.edu>
2021-01-27 19:19:30 -0800

    Moved a test

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -47,6 +47,24 @@ namespace CanHazFunny.Tests
         }
 
         [TestMethod]
+        public void Jester_PassJokeToTellJoke_SendsCorrectJokeToPrintJoke()
+        {
+            //Assign
+            Mock<IJokeService> mockJokeService = new Mock<IJokeService>();
+            _ = mockJokeService.SetupSequence(jokeService => jokeService.GetJoke())
+                .Returns("Corny dad joke");
+
+            Mock<IJokeOutput> mockJokeOutput = new Mock<IJokeOutput>();
+            _ = mockJokeOutput.SetupSequence(jokeOutput => jokeOutput.PrintJoke("Corny dad joke"));
+
+            //Act
+            new Jester(mockJokeService.Object, mockJokeOutput.Object).TellJoke();
+
+            //Assert
+            mockJokeOutput.VerifyAll();
+        }
+
+        [TestMethod]
         public void Jester_TellJokeFiltersOutChuckNorris_ReturnsJokesWithoutChuckNorris()
         {
             //Assign
