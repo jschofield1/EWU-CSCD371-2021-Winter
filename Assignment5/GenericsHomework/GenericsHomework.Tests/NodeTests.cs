4dceea25f1b1351e166e47215a820a81c66a5e87
James Schofield <jschofield1@eagles.ewu.edu>
2021-02-09 04:14:01 -0800

    Organized tests better

--- Assignment5/GenericsHomework/GenericsHomework.Tests/NodeTests.cs
+++ Assignment5/GenericsHomework/GenericsHomework.Tests/NodeTests.cs
@@ -7,6 +7,24 @@ namespace GenericsHomework.Tests
     public class NodeTests
     {
         [TestMethod]
+        public void GetArrayLength_CreateMultipleNodes_ArrayEqualsExpectedLength()
+        {
+            Node<int> node = new Node<int>(1);
+            
+            node.Insert(2);
+
+            Assert.AreEqual(2, node.Count);
+        }
+
+        [TestMethod]
+        public void IsReadOnly_ChecksIfNodeIsReadOnly_ReturnsFalse_()
+        {
+            Node<int> node = new Node<int>(1);
+            
+            Assert.IsFalse(node.IsReadOnly);
+        }
+
+        [TestMethod]
         public void Node_PassesDifferentDataTypes_NodesWithDifferentTypesAreCreated()
         {
             Node<int> first = new Node<int>(1);
@@ -18,6 +36,24 @@ namespace GenericsHomework.Tests
             Assert.AreEqual<double>(2.0, second.Data);
             Assert.AreEqual<string>("three", third.Data);
             Assert.AreEqual<char>('4', fourth.Data);
+        }
+
+        [TestMethod]
+        public void Node_CreateTwoNodesWithDifferentValues_NodesAreNotEqual()
+        {
+            Node<int> first = new Node<int>(1);
+            Node<int> second = new Node<int>(2);
+
+            Assert.AreNotEqual<Node<int>>(first, second);
+        }
+
+        [TestMethod]
+        public void Node_ConstructNewNodeWithNext_ConstructsNextNodeAfterHead()
+        {
+            Node<int> node = new Node<int>(1);
+            Node<int> nextNode = node.Next;
+
+            Assert.AreEqual<Node<int>>(node, nextNode);
         }
 
         [TestMethod]
@@ -49,25 +85,7 @@ namespace GenericsHomework.Tests
         }
 
         [TestMethod]
-        public void Node_CreateTwoNodesWithDifferentValues_NodesAreNotEqual()
-        {
-            Node<int> first = new Node<int>(1);
-            Node<int> second = new Node<int>(2);
-
-            Assert.AreNotEqual<Node<int>>(first, second);
-        }
-
-        [TestMethod]
-        public void Node_ConstructNewNodeWithNext_ConstructsNextNodeAfterHead()
-        {
-            Node<int> node = new Node<int>(1);
-            Node<int> nextNode = node.Next;
-
-            Assert.AreEqual<Node<int>>(node, nextNode);
-        }
-
-        [TestMethod]
-        public void Insert_InsertNewNodeonHead_NextOnLastNodeLoopsToHead()
+        public void Insert_InsertNewNodeOnHead_NextOnLastNodeLoopsToHead()
         {
             Node<int> node = new Node<int>(1);
             
@@ -89,6 +107,56 @@ namespace GenericsHomework.Tests
 
             Assert.AreEqual<int>(1, node.Data);
             Assert.AreEqual<int>(1, node.Next.Data);
+        }
+        
+        [TestMethod]
+        public void Contains_ChecksNodeForSpecificValue_ReturnsTrue()
+        {
+            Node<int> node = new Node<int>(1);
+            
+            Assert.IsTrue(node.Contains(1));
+        }
+       
+        [TestMethod]
+        public void CopyTo_CopyValuesToArray_ArrayHoldsExpectedValues()
+        {
+            int[] copiedArray = new int[3];
+            int[] expectedArray = { 1, 2, 3 };
+
+            Node<int> node = new Node<int>(1);
+
+            node.Insert(2);
+            node.Insert(3);
+            node.CopyTo(copiedArray, 0);
+
+            Assert.AreEqual(expectedArray.ToString(), copiedArray.ToString());
+        }
+        
+        [TestMethod]
+        public void Remove_RemoveSpecificValue_ExpectedValueRemoved()
+        {
+            Node<int> node = new Node<int>(1);
+
+            node.Insert(2);
+            node.Insert(3);
+            node.Remove(2);
+
+            Assert.AreEqual("1 3", $"{node.ToString()} {node.Next.ToString()}");
+        }
+        
+        [TestMethod]
+        public void GetEnumerator_ForeachToIterate_AddsExpectedValues()
+        {
+            Node<int> first = new Node<int>(1);
+            Node<int> second = new Node<int>(2);
+
+            //node.Insert(2);
+
+            foreach (int item in first)
+            {
+                second.Add(item);
+            }
+            Assert.AreEqual("2 1", $"{second.ToString()} {second.Next.ToString()}");
         }
     }
 }
