c8df231cd13bd888c9a29c44a96a6157de2430aa
James Schofield <jschofield1@eagles.ewu.edu>
2021-03-09 01:43:55 -0800

    Made some small adjustments

--- Assignment8/Assignment8/Assignment8/MainWindow.xaml.cs
+++ Assignment8/Assignment8/Assignment8/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace Assignment8
@@ -35,7 +24,6 @@ namespace Assignment8
 
             dispatcherTimer.Tick += new EventHandler(DispatcherTimer_Tick);
             dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
-
         }
 
         private void DispatcherTimer_Tick(object sender, EventArgs e)
@@ -50,21 +38,20 @@ namespace Assignment8
                 _StartTime = DateTime.Now;
                 dispatcherTimer.Start();
                 _IsStopped = false;
-                Record.Visibility = Visibility.Hidden;
+                Save.Visibility = Visibility.Hidden;
                 Description.Visibility = Visibility.Hidden;
                 StartAndStop.Content = "Stop";
-                StartAndStop.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#E14518");
+                StartAndStop.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#ff0000");
             }
             else
             {
                 dispatcherTimer.Stop();
                 _IsStopped = true;
                 StartAndStop.Content = "Start";
-                StartAndStop.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#6FA147");
-                Record.Visibility = Visibility.Visible;
+                StartAndStop.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#00cc00");
+                Save.Visibility = Visibility.Visible;
                 Description.Visibility = Visibility.Visible;
-                Description.Text = "Time " + count + " (" + _StartTime.ToString("hh:mm:ss") + ")";
-                //Description.Text = "Time " + count + " - ";
+                Description.Text = "Event " + count + " (" + _StartTime.ToString("MMM dd") + ", " + _StartTime.ToString("hh:mm tt") + ")";
             }
         }
 
@@ -72,13 +59,11 @@ namespace Assignment8
         {
             if (_IsStopped && Timer.Text != "00:00:00")
             {
-                //SavedBox.Items.Add($"{Timer.Text}\t{_Description}");
-                //SavedBox.Items.Add($"{_Description}\t{Timer.Text}");
-                SavedBox.Items.Add($"{_Description}\t{" - "}\t{Timer.Text}");
+                SavedBox.Items.Add($"{_Description}{" - "}{Timer.Text}");
                 count++;
                 Description.Text = "";
                 Timer.Text = "00:00:00";
-                Record.Visibility = Visibility.Hidden;
+                Save.Visibility = Visibility.Hidden;
                 Description.Visibility = Visibility.Hidden;
             }
         }
